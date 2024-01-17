e9515335a2cdf8363d5e2a9e83fc27ecd4b25fb8
soloKomrade <136370929+Brian-Itira@users.noreply.github.com>
2024-01-17 13:43:09 +0000

    added google fonts

--- src/javascript/index.mjs
+++ src/javascript/index.mjs
@@ -1,5 +1,5 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>this is javascript and its awesome right  </h1>
+<h1>this is javascript and its working well and this is awesome</h1>
 `;
