638fbd24111bd7a890625abe5f4fef8ce7cf22cb
soloKomrade <136370929+Brian-Itira@users.noreply.github.com>
2024-01-17 13:55:21 +0000

    fixing relative paths in the scr folder

--- src/javascript/index.mjs
+++ src/javascript/index.mjs
@@ -1,5 +1,6 @@
-import "./styles.css";
+import * as bootstrap from "bootstrap";
+import "../scss/styles.scss";
 
 document.getElementById("app").innerHTML = `
-<h1>this is javascript and its working well and this is awesome</h1>
+<h1>this is javascript and its working well and this is awesome and so much fun</h1>
 `;
