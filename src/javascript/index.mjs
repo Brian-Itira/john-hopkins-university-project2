02f298986177c0381bebf5ad376e512f81e1cd82
soloKomrade <136370929+Brian-Itira@users.noreply.github.com>
2024-01-22 06:51:02 +0000

    implementing the navbar of the resturant

--- src/javascript/index.mjs
+++ src/javascript/index.mjs
@@ -1,6 +1,2 @@
 import * as bootstrap from "bootstrap";
 import "../scss/styles.scss";
-
-document.getElementById("app").innerHTML = `
-<h1>this is javascript and its working well and this is awesome and so much fun</h1>
-`;
